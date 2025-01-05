51ba76481b7961d1994bfff554de6057b185adbe
h-makihara <macky0325@gmail.com>
2025-01-05 11:37:46 +0900

    Update: クエリパラメータのデフォルト値実装（Annotatedでの実装）

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from typing import Annotated
+
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: str = Query(default="fixedquery", min_length=3)):
+async def read_items(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
