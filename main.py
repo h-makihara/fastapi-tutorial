1e3511491c0c0b540533ac9b40b961294f72842f
h-makihara <macky0325@gmail.com>
2025-01-05 11:08:19 +0900

    Update: ボディ・パス・クエリの同時宣言を実装

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
