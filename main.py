f1d017916bc9b909f0ab81f67d0ac077c5bd2a86
h-makihara <macky0325@gmail.com>
2025-01-05 15:00:18 +0900

    Update: 追加の余分なフィールドを禁止する定義を実装

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from typing import Annotated, Literal
+
 from fastapi import FastAPI, Query
 from pydantic import BaseModel, Field
-from typing_extensions import Annotated, Literal
 
 app = FastAPI()
 
 
 class FilterParams(BaseModel):
+    model_config = {"extra": "forbid"}
+
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
